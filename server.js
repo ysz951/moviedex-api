4de73e79bd8d4e1f8a3b8adaf70eb70c0788e751
ysz951 <shengyang_zhou@baylor.edu>
2020-07-15 19:11:15 -0500

    prepare for deploying

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const helmet = require('helmet')
 const MOVIEDEX = require('./movies-data-small.json')
 const app = express()
 
-app.use(morgan('dev'))
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -41,7 +43,17 @@ app.get('/movie', (req, res) => {
     res.json(response)
 })
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+    let response
+    if (process.env.NODE_ENV === 'production') {
+      response = { error: { message: 'server error' }}
+    } else {
+      response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
