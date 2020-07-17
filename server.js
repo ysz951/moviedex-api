1689ddebaa07e509483c92c17aa360847d682785
ysz951 <shengyang_zhou@baylor.edu>
2020-07-16 22:04:55 -0500

    fix bug

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const MOVIEDEX = require('./movies-data-small.json')
 const app = express()
 
 
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
-app.use(morgan(morganSetting))
+// const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+// app.use(morgan(morganSetting))
+app.use(morgan('dev'))
 app.use(helmet())
 app.use(cors())
 
@@ -37,21 +38,21 @@ app.get('/movie', (req, res) => {
     }
     if (avg_vote) {
         response = response.filter(movie => 
-            movie.avg_vote >= Number(avg_vote)
+            Number(movie.avg_vote) >= Number(avg_vote)
         );
     }
     res.json(response)
 })
 
-app.use((error, req, res, next) => {
-    let response
-    if (process.env.NODE_ENV === 'production') {
-      response = { error: { message: 'server error' }}
-    } else {
-      response = { error }
-    }
-    res.status(500).json(response)
-})
+// app.use((error, req, res, next) => {
+//     let response
+//     if (process.env.NODE_ENV === 'production') {
+//       response = { error: { message: 'server error' }}
+//     } else {
+//       response = { error }
+//     }
+//     res.status(500).json(response)
+// })
 
 const PORT = process.env.PORT || 8000
 
